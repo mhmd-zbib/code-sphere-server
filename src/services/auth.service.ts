bd1a7eff150202ab48a2077492b6620d823e610e
moe-zbib <mohammad.h.zbib@gmail.com>
2024-05-26 17:15:07 +0300

    jwt

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,12 +1,20 @@
 import { PrismaClient } from "@prisma/client";
 import { userModel } from "../models/user";
 import { hashString } from "../utils/hash";
+import { generateAccessToken } from "../utils/jwt";
 const userClient = new PrismaClient().user;
 
 async function signup(username: string, email: string, password: string) {
   const hashedPassword = await hashString(password);
   const user = await userModel.create(username, email, hashedPassword);
-  return user;
+
+  const accessToken = generateAccessToken({
+    id: user.id,
+    username: username,
+    email: email,
+  });
+
+  return accessToken;
 }
 
 export const authService = {
---
+++ src/utils/jwt.ts
@@ -0,0 +1,40 @@
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+const ACCESS_TOKEN_EXPIRY = "120m";
+const REFRESH_TOKEN_EXPIRY = "20d";
+export const TOKEN_SECRET: string = process.env.ACCESS_TOKEN!;
+
+interface DecodedToken extends JwtPayload {}
+
+export function generateAccessToken(payload: any): string {
+  console.log(ACCESS_TOKEN_EXPIRY);
+
+  return jwt.sign(payload, TOKEN_SECRET, { expiresIn: ACCESS_TOKEN_EXPIRY });
+}
+
+export function generateRefreshToken(
+  payload: any,
+  expiresIn: string = REFRESH_TOKEN_EXPIRY
+): string {
+  return jwt.sign(payload, TOKEN_SECRET, { expiresIn: ACCESS_TOKEN_EXPIRY });
+}
+
+export function verifyAccessToken(token: string): DecodedToken | null {
+  try {
+    const decoded = jwt.verify(token, TOKEN_SECRET) as DecodedToken;
+    return decoded;
+  } catch (err: any) {
+    console.error("Access token verification failed:", err.message);
+    return null;
+  }
+}
+
+export function verifyRefreshToken(token: string): DecodedToken | null {
+  try {
+    const decoded = jwt.verify(token, TOKEN_SECRET) as DecodedToken;
+    return decoded;
+  } catch (err: any) {
+    console.error("Refresh token verification failed:", err.message);
+    return null;
+  }
+}
